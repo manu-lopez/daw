42d991ed48a3afb7db5579e81e25e4022242aead
manu-lopez <manuellp87@gmail.com>
2019-11-05 22:17:56 +0100

    Añade ejercicio 15 tanda 4 DWES

--- DWES/Tema 2/Ejercicios/Tanda 4 (T4)/15.php
+++ DWES/Tema 2/Ejercicios/Tanda 4 (T4)/15.php
@@ -1,19 +1,19 @@
 <!-- Realiza un programa que sea capaz de rotar todos los elementos de una matriz cuadrada una posición en el sentido de las agujas del reloj. La matriz debe tener 12 filas por 12 columnas y debe contener números generados al azar entre 0 y 100. Se debe mostrar tanto la matriz original como la matriz resultado, ambas con los números convenientemente alineados -->
 <?php
-$numeroFilas = 3;
-$numerosColumnas = 3;
+$numeroFilas = $numerosColumnas = 5;
 $fila = $columna = 0;
 
 echo "<p> MATRIZ ORIGINAL </p>";
 for ($i = 0; $i < $numeroFilas; $i++) {
   for ($k = 0; $k < $numerosColumnas; $k++) {
-    // $array[$i][$k] = rand(0, 100);
-    $array[$i][$k] = $k;
+    $array[$i][$k] = rand(0, 100);
+    // $array[$i][$k] = $k; // rellenamos con numero consecutivos, para facilitar desarrollo
     echo $array[$i][$k] . " ";
   }
   echo "<br> ";
 }
 echo "<p> MATRIZ ROTADA </p>";
+$nuevoArray = $array;
 while ($fila < $numeroFilas and $columna < $numerosColumnas) {
 
   //Primera fila
@@ -21,7 +21,7 @@ while ($fila < $numeroFilas and $columna < $numerosColumnas) {
   //Guardamos primer valor de la fila siguiente
   $valorPrevio = $array[$fila + 1][$columna];
 
-  for ($i = $fila; $i < $numerosColumnas; $i++) {
+  for ($i = $columna; $i < $numerosColumnas; $i++) {
     $valorActual = $array[$fila][$i];
     $nuevoArray[$fila][$i] = $valorPrevio;
     $valorPrevio = $valorActual;
@@ -38,7 +38,31 @@ while ($fila < $numeroFilas and $columna < $numerosColumnas) {
 
   //Ultima fila
   if ($fila < $numeroFilas) {
-    for ($i = $numerosColumnas - 1; $i >= $columna; $i--) { }
+    for ($i = $numerosColumnas - 1; $i >= $columna; $i--) {
+      $valorActual = $array[$numeroFilas - 1][$i];
+      $nuevoArray[$numeroFilas - 1][$i] = $valorPrevio;
+      $valorPrevio = $valorActual;
+    }
   }
+  $numeroFilas--;
+
+  //Primera Columna
+  if ($columna < $numerosColumnas) {
+    for ($i = $numeroFilas - 1; $i >= $fila; $i--) {
+      $valorActual = $array[$i][$columna];
+      $nuevoArray[$i][$columna] = $valorPrevio;
+      $valorPrevio = $valorActual;
+    }
+  }
+  $columna++;
+}
+
+//Mostramos nuevo array
+
+for ($i = 0; $i < count($array); $i++) {
+  for ($k = 0; $k < count($array); $k++) {
+    echo $nuevoArray[$i][$k] . " ";
+  }
+  echo "<br>";
 }
 ?>
