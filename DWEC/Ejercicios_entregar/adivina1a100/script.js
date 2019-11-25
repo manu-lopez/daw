b04775f3956a5e8a811095f2936b9077e2300bb2
manu-lopez <manuellp87@gmail.com>
2019-11-25 19:59:11 +0100

    Añade modificaiones a juego adivinar DWEC

--- DWEC/Ejercicios_entregar/adivina1a100/script.js
+++ DWEC/Ejercicios_entregar/adivina1a100/script.js
@@ -1,22 +1,42 @@
-let numeroAletaorio = Math.round(Math.random() * 100);
+let numeroAletaorio;
 let pnumeroacertar = document.getElementById("numeroacertar");
+let resultado = document.getElementById("resultado");
+let botonIniciar = document.getElementById("inicializarJuego");
+let botonComprobar = document.getElementById("botonComprobar");
 let limiteInferior = 1;
 let limiteSuperior = 100;
 
-pnumeroacertar.innerHTML = numeroAletaorio;
+
 
 const comprobarNumero = () => {
   let numeroUsuario = parseInt(document.getElementById("numeroUsuario").value);
-  let resultado = document.getElementById("resultado");
   if (numeroUsuario == numeroAletaorio) {
+    resultado.classList.remove("text-info");
+    resultado.classList.add("text-success");
     resultado.innerHTML = "¡Enhorabuena has acertado!";
+    botonComprobar.disabled = true;
+    botonIniciar.style.visibility = "visible";
   } else {
     if (numeroUsuario < numeroAletaorio) {
       limiteInferior = (numeroUsuario + 1);
       resultado.innerHTML = "El número esta comprendido entre " + limiteInferior + " y " + limiteSuperior;
     } else {
-      limiteSuperior = (numeroUsuario - 1);
       resultado.innerHTML = "El número esta comprendido entre " + limiteInferior + " y " + limiteSuperior;
+      limiteSuperior = (numeroUsuario - 1);
     }
   }
 }
+
+const iniciarlizarJuego = () => {
+  botonIniciar.style.visibility = "hidden";
+  numeroAletaorio = Math.round(Math.random() * 100);
+  botonComprobar.disabled = false;
+  resultado.innerHTML = "";
+  resultado.classList.add("text-info");
+  // pnumeroacertar.innerHTML = numeroAletaorio;
+  limiteInferior = 1;
+  limiteSuperior = 100;
+}
+
+iniciarlizarJuego();
+// pnumeroacertar.innerHTML = numeroAletaorio;
