f22a86c4993b06671340e096d5153048c676f3f6
Manu <manuellp87@gmail.com>
2019-12-11 18:55:44 +0100

    Añadido options en crud DWES

--- DWES/Tema_3/CRUD/mostrarAgregar.php
+++ DWES/Tema_3/CRUD/mostrarAgregar.php
@@ -20,17 +20,34 @@
             $query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'dwes' AND TABLE_NAME = '".strtolower($_POST["categoria"])."'";
             $queryColumnas = $dwes->query($query);
             if($queryColumnas){
-
-                //TODO option en stock con cod y en producto con familia
                 echo '<form action="agregar.php" method="POST">';
                 echo '<input type="hidden" name="tabla" value="'.strtolower($_POST["categoria"]).'">';
+                
                 while($r = $queryColumnas->fetch_assoc()){
-                    if ($r["COLUMN_NAME"] == 'descripcion') {
-                        echo $r["COLUMN_NAME"].'<textarea name="'.$r["COLUMN_NAME"].'" rows="4" cols="50"></textarea>';
-                    }else{
-                        echo $r["COLUMN_NAME"].'<input type="text" name="'.$r["COLUMN_NAME"].'" value="">';
+                    if (strtolower($_POST["categoria"]) == 'stock' and $r["COLUMN_NAME"] == 'cod') {
+                        $queryCodStock = 'SELECT cod FROM producto'; // lo mostramos en stock -> cod
+                        $codStock = $dwes->query($queryCodStock);
+                        echo $r["COLUMN_NAME"].'<select name="cod" id="cod">';
+                        while($cod = $codStock->fetch_assoc()){
+                            echo '<option value="'.$cod["cod"].'">'.$cod["cod"].'</option>';
+                        }
+                        echo '</select><br>';
+                    } elseif (strtolower($_POST["categoria"]) == 'producto' and $r["COLUMN_NAME"] == 'familia') {
+                        $queryCodFamilia = 'SELECT cod FROM familia'; // lo mostramos en producto -> familia
+                        $codFamilia = $dwes->query($queryCodFamilia);
+                        echo $r["COLUMN_NAME"].'<select name="cod" id="cod">';
+                        while($cod = $codFamilia->fetch_assoc()){
+                            echo '<option value="'.$cod["cod"].'">'.$cod["cod"].'</option>';
+                        }
+                        echo '</select><br>';
+                    }else {
+                        if ($r["COLUMN_NAME"] == 'descripcion') {
+                            echo $r["COLUMN_NAME"].'<textarea name="'.$r["COLUMN_NAME"].'" rows="4" cols="50"></textarea>';
+                        }else{
+                            echo $r["COLUMN_NAME"].'<input type="text" name="'.$r["COLUMN_NAME"].'" value="">';
+                        }
+                        echo '<br>';
                     }
-                    echo '<br>';
                 }
                 echo '<br><input type="submit" value="Agregar">';
                 echo '</form>';
