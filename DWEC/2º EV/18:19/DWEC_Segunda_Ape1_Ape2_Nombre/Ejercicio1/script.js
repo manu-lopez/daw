7536e6d6235e4f1df6f96bf1ba7ceab48cc4c84c
Manu <manuellp87@gmail.com>
2020-02-03 18:17:17 +0100

    Modifica DWEC
    Primer ejercicio del segundo examen

--- DWEC/2º EV/18:19/DWEC_Segunda_Ape1_Ape2_Nombre/Ejercicio1/script.js
+++ DWEC/2º EV/18:19/DWEC_Segunda_Ape1_Ape2_Nombre/Ejercicio1/script.js
@@ -1,4 +1,4 @@
-let palabrasJSON = JSON.parse(palabras);
+let palabrasJSON = JSON.parse(palabras).sort();
 let button = document.getElementById("button");
 let vocales = new RegExp(/\b(?=\w*?a)(?=\w*?e)(?=\w*?i)(?=\w*?o)(?=\w*?u)[a-zA-Z]+\b/);
 
@@ -8,14 +8,18 @@ const mostrarInfo = () => {
     //Contamos cantidad de palabras en diccionario.js
     let cantidadPalabras = palabrasJSON.length;
 
+
     // Contamos palabras con todas las vocales
+    let palabrasCincoVocales = []
     palabrasJSON.forEach(palabra => {
         if (vocales.test(palabra)) {
+            palabrasCincoVocales.push("\n" + palabra);
             contador++;
         }
     });
 
-    alert(`Palabras totales: ${cantidadPalabras}. \nPalabras con vocales: ${contador}.`);
+
+    alert(`Palabras totales: ${cantidadPalabras}. \nPalabras con vocales: ${contador}. \n${palabrasCincoVocales}`);
 }
 
 button.addEventListener("click", mostrarInfo);
